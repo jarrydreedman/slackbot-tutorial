262138130fdcb96d9d34930895983798b8c161fd
jarrydreedman <jarrydreedman@gmail.com>
2019-04-15 14:57:41 +1000

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, make_response, Response
-import json
+import json, os
 from tokens import SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN
 from slackclient import SlackClient
 from slashCommand import *
-
-
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 app = Flask(__name__)
@@ -13,16 +11,24 @@ commander = Slash("Hey there! It works.")
 
 #TODO: Add checks for all responses from slack api calls
 
+
 def verify_slack_token(request_token):
     if SLACK_VERIFICATION_TOKEN != request_token:
         print("Error: invalid verification token!")
         print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
         return make_response("Request contains invalid Slack verification token", 403)
 
+
+@app.route("/")
+def home():
+  return 'yo'
+
 @app.route("/slack/test", methods=["POST"])
 def command():
   info = request.form
+  
 
+  
   # # get uid of the user
   # im_id = slack_client.api_call(
   #   "im.open",
@@ -36,11 +42,10 @@ def command():
   #   text=commander.getMessage()
 
   # send channel a response
-  channelMsg = slack_client.api_call(
-    "chat.postMessage",
-    channel="#" + info["channel_name"],
-    text=commander.getMessage()
-
+  
+  
+  channelMsg = slack_client.api_call("chat.postMessage",channel="#" + info["channel_name"],text=commander.getMessage())
+ 
   return make_response("", 200)
 
 # Start the Flask server
